46d6131a8cda0483a32c0761854404c21cc7ad49
alessandro.zampella <aleszamp98@gmail.com>
2025-11-18 20:52:28 +0100

    setup file modified to allow installation without errors.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup, find_packages
- 
+from setuptools import setup
+
 setup(
     name='ProMeteo',
     version='1.0.0',
-    packages=find_packages(where="src"),
     package_dir={"": "src"},
-    )
+    py_modules=['core', 'pre_processing', 'frame'],
+    install_requires=[
+        'numpy',
+        'pandas',
+    ],
+    author='Alessandro Zampella',
+    description='ProMeteo (PROcessing of METEOrological Data) is a Python library for preprocessing and manipulating measurements collected by sonic anemometers mounted on meteorological towers.',
+    url='https://github.com/aleszamp98/ProMeteo',
+)   
